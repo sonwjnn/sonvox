3c3b11abe0a314e9bbc545389ef605fbb6f5e7de
sonwjnn <nguyenlehoangson2106@gmail.com>
2026-03-20 11:48:39 +0700

    initial commit

---
+++ packages/auth/src/index.ts
@@ -0,0 +1,42 @@
+import prisma from "@demo-new-feature/db";
+import { env } from "@demo-new-feature/env/server";
+import { polar, checkout, portal } from "@polar-sh/better-auth";
+import { betterAuth } from "better-auth";
+import { prismaAdapter } from "better-auth/adapters/prisma";
+import { tanstackStartCookies } from "better-auth/tanstack-start";
+
+import { polarClient } from "./lib/payments";
+
+export const auth = betterAuth({
+  database: prismaAdapter(prisma, {
+    provider: "postgresql",
+  }),
+
+  trustedOrigins: [env.CORS_ORIGIN],
+  emailAndPassword: {
+    enabled: true,
+  },
+  secret: env.BETTER_AUTH_SECRET,
+  baseURL: env.BETTER_AUTH_URL,
+  plugins: [
+    polar({
+      client: polarClient,
+      createCustomerOnSignUp: true,
+      enableCustomerPortal: true,
+      use: [
+        checkout({
+          products: [
+            {
+              productId: "your-product-id",
+              slug: "pro",
+            },
+          ],
+          successUrl: env.POLAR_SUCCESS_URL,
+          authenticatedUsersOnly: true,
+        }),
+        portal(),
+      ],
+    }),
+    tanstackStartCookies(),
+  ],
+});
---
+++ packages/db/src/index.ts
@@ -0,0 +1,12 @@
+import { env } from "@demo-new-feature/env/server";
+import { PrismaPg } from "@prisma/adapter-pg";
+
+import { PrismaClient } from "../prisma/generated/client";
+
+const adapter = new PrismaPg({
+  connectionString: env.DATABASE_URL,
+});
+
+const prisma = new PrismaClient({ adapter });
+
+export default prisma;
